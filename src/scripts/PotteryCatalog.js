4ffc16fab64b68db2a2715930e65c46f26a85a3c
Kolby Simons <kolby.simons@gmail.com>
2025-05-23 11:08:57 -0500

    Added PotteryCatalog.js

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,23 @@
+//define a variable with the value of an empty array
+let potteryToSell = [];
+//declare and export a function called toSellOrNotToSell that will determine if a piece of pottery should be sold or not
+//should accept a pottery object as an input
+//if the weight is greater than or equal to 6 the function should add a price property with the value of 40, less than 6 should be 20
+//do not add a price for cracked items
+// if not cracked add to the module-level array to be sold
+// return the augmented object
+// define and export a function called usePottery that returns a copy of the array to be sold.
+
+export const toSellOrNotToSell = (shouldISell) => {
+  if (shouldISell.weight >= 6 && !shouldISell.cracked) {
+    shouldISell.price = 40;
+  } else if (shouldISell.weight < 6 && !shouldISell.cracked) {
+    shouldISell.price = 20;
+    potteryToSell.push(shouldISell);
+  }
+  return shouldISell;
+};
+
+export const usePottery = () => {
+  return structuredClone(potteryToSell);
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,22 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js";
 import { firePottery } from "./Kiln.js";
+import { toSellOrNotToSell } from "./PotteryCatalog.js";
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 1, 5);
-console.log(mug);
 let plate = makePottery("Plate", 2, 1);
-console.log(plate);
 let bowl = makePottery("Bowl", 3, 6);
-console.log(bowl);
 let vase = makePottery("Vase", 8, 12);
-console.log(vase);
 let pitcher = makePottery("Pitcher", 5, 8);
-console.log(pitcher);
-// Fire each piece of pottery in the kiln
-let firedMug = firePottery(2300, mug);
-console.log(firedMug);
-let firedPlate = firePottery(2300, plate);
-console.log(firedPlate);
-let firedBowl = firePottery(2300, bowl);
-console.log(firedBowl);
-let firedVase = firePottery(2300, vase);
-console.log(firedVase);
-let firedPitcher = firePottery(2300, pitcher);
-console.log(firedPitcher);
-// Determine which ones should be sold, and their price
 
+// Fire each piece of pottery in the kiln
+let firedMug = firePottery(2100, mug);
+let firedPlate = firePottery(2200, plate);
+let firedBowl = firePottery(2400, bowl);
+let firedVase = firePottery(2100, vase);
+let firedPitcher = firePottery(2200, pitcher);
+
+// Determine which ones should be sold, and their price
+let toSell = toSellOrNotToSell(firedMug);
+console.log(toSell);
 // Invoke the component function that renders the HTML list
